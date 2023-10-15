aa1cc7e6a56305f375eac21d1a9382a6038e3173
yanpatla <tomipatlayan@gmail.com>
2023-10-15 18:52:38 +0300

    CRON task

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,7 +1,19 @@
-
+import { CronJob } from "cron";
 
 export class Server {
   public static start() {
     console.log("Server started...");
+
+    const job = new CronJob(
+      "* * * * * *",
+      () => {
+        console.log("You will see this message every second");
+      },
+      null,
+      true,
+      "America/Los_Angeles"
+    );
+
+    job.start();
   }
 }

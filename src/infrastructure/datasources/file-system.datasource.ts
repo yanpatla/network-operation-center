58a6f8a0ffee88a07da96b400d58b1a94a3f09cb
yanpatla <tomipatlayan@gmail.com>
2023-10-16 20:16:07 +0300

    FileSystem SaveLog

--- src/infrastructure/datasources/file-system.datasource.ts
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -4,7 +4,7 @@ import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
 
 export class FileSystemDataSource implements LogDatasource {
   private readonly logPath = "logs/";
-  private readonly allLogsPath = "logs/logs-low.log";
+  private readonly allLogsPath = "logs/logs-all.log";
   private readonly mediumLogsPath = "logs/logs-medium.log";
   private readonly highLogsPath = "logs/logs-high.log";
 
@@ -24,8 +24,18 @@ export class FileSystemDataSource implements LogDatasource {
     );
   };
 
-  saveLogs(log: LogEntity): Promise<void> {
-    throw new Error("Method not implemented.");
+  async saveLogs(newLog: LogEntity): Promise<void> {
+    const logAsJson = `${JSON.stringify(newLog)}\n`;
+
+    fs.appendFileSync(this.allLogsPath, logAsJson);
+
+    if (newLog.level === LogSeverityLevel.low) return;
+
+    if (newLog.level === LogSeverityLevel.medium) {
+      fs.appendFileSync(this.mediumLogsPath, logAsJson);
+    } else {
+      fs.appendFileSync(this.highLogsPath, logAsJson);
+    }
   }
   getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
     throw new Error("Method not implemented.");

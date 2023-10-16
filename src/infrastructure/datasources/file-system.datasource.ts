d975565a6dec79518a73f68b971a25ec966cc353
yanpatla <tomipatlayan@gmail.com>
2023-10-16 19:46:52 +0300

    FileSystem DataSource

---
+++ src/infrastructure/datasources/file-system.datasource.ts
@@ -0,0 +1,33 @@
+import fs from "fs";
+import { LogDatasource } from "../../domain/datasources/log.datasource";
+import { LogEntity, LogSeverityLevel } from "../../domain/entities/log.entity";
+
+export class FileSystemDataSource implements LogDatasource {
+  private readonly logPath = "logs/";
+  private readonly allLogsPath = "logs/logs-low.log";
+  private readonly mediumLogsPath = "logs/logs-medium.log";
+  private readonly highLogsPath = "logs/logs-high.log";
+
+  constructor() {
+    this.createLogsFiles();
+  }
+
+  private createLogsFiles = () => {
+    if (!fs.existsSync(this.logPath)) {
+      fs.mkdirSync(this.logPath);
+    }
+    [this.allLogsPath, this.mediumLogsPath, this.highLogsPath].forEach(
+      (path) => {
+        if (fs.existsSync(path)) return;
+        fs.writeFileSync(path, "");
+      }
+    );
+  };
+
+  saveLogs(log: LogEntity): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+  getLogs(severityLevel: LogSeverityLevel): Promise<LogEntity[]> {
+    throw new Error("Method not implemented.");
+  }
+}

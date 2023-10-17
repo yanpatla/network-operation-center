4166a138b0f331717e7a7a2067734679bf4c2e9b
yanpatla <tomipatlayan@gmail.com>
2023-10-17 22:07:32 +0300

    trying implemention

--- src/domain/use-cases/checks/checks-service.ts
+++ src/domain/use-cases/checks/checks-service.ts
@@ -1,3 +1,6 @@
+import { LogEntity, LogSeverityLevel } from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+
 interface ICheckServiceUseCase {
   execute(url: string): Promise<boolean>;
 }
@@ -7,6 +10,7 @@ type ErrorCallback = (error: string) => void;
 
 export class CheckService implements ICheckServiceUseCase {
   constructor(
+    private readonly logRepository: LogRepository,
     private readonly successCallback: SuccessCallback,
     private readonly errorCallback: ErrorCallback
   ) {}
@@ -15,11 +19,17 @@ export class CheckService implements ICheckServiceUseCase {
       const req = fetch(url);
       if (!(await req).ok) throw new Error(`Error on check service ${url}`);
 
+      const log = new LogEntity(`Service ${url} working`, LogSeverityLevel.low);
+      this.logRepository.saveLogs(log);
       this.successCallback();
+
       return true;
     } catch (error) {
-      console.log(`${error} `);
-      this.errorCallback(`${error}`);
+      const errorMessage = `${url} is not ok. ${error}`;
+      const log = new LogEntity(errorMessage, LogSeverityLevel.high);
+      this.logRepository.saveLogs(log);
+
+      this.errorCallback(errorMessage);
       return false;
     }
   }
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,18 +1,24 @@
 import { CheckService } from "../domain/use-cases/checks/checks-service";
+import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
+import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository.impl";
 import { CronService } from "./cron/cron-service";
+
+const fileSystemLogRepository = new LogRepositoryImpl(
+  new FileSystemDataSource()
+);
 
 export class Server {
   public static start() {
     console.log("Server started...");
 
     CronService.createJob("*/5 * * * * *", () => {
-      const url: string = "https://google.com";
+      const url: string = "http://localhost:3000";
 
       new CheckService(
+        fileSystemLogRepository,
         () => console.log(`${url} is ok`),
         (error) => console.log(error)
       ).execute(url);
-      //   new CheckService().execute("http://localhost:3000");
     });
   }
 }

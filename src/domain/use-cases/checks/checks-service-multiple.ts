93da477868efcee78e9b7acaa265253e134e429b
yanpatla <tomipatlayan@gmail.com>
2023-11-04 18:41:58 +0200

    check multiples services

---
+++ src/domain/use-cases/checks/checks-service-multiple.ts
@@ -0,0 +1,66 @@
+import fs from "fs";
+import * as path from "path";
+import {
+  LogEntity,
+  LogEntityOptions,
+  LogSeverityLevel,
+} from "../../entities/log.entity";
+import { LogRepository } from "../../repository/log.repository";
+interface ICheckServiceMultipleUseCase {
+  execute(url: string): Promise<boolean>;
+}
+
+type SuccessCallback = (() => void) | undefined;
+type ErrorCallback = ((error: string) => void) | undefined;
+
+export class CheckServiceMultiple implements ICheckServiceMultipleUseCase {
+  constructor(
+    private readonly logRepository: LogRepository[],
+    private readonly successCallback: SuccessCallback,
+    private readonly errorCallback: ErrorCallback
+  ) {}
+
+  private callLogs(log: LogEntity) {
+    this.logRepository.forEach((logRepository) => {
+      logRepository.saveLogs(log);
+    });
+  }
+  public async execute(url: string): Promise<boolean> {
+    // Get the absolute path of the current file
+    const currentFilePath = __filename;
+
+    // Use fs.realpathSync to get the real (absolute) path
+    const realFilePath = fs.realpathSync(currentFilePath);
+
+    // Extract only the filename from the path
+    const currentFileName = path.basename(realFilePath);
+    try {
+      const req = fetch(url);
+      if (!(await req).ok) throw new Error(`Error on check service ${url}`);
+      const message = `Service ${url} working`;
+
+      const options: LogEntityOptions = {
+        level: LogSeverityLevel.low,
+        message: message,
+        origin: currentFileName,
+      };
+      const log = new LogEntity(options);
+      this.callLogs(log);
+      this.successCallback && this.successCallback();
+
+      return true;
+    } catch (error) {
+      const errorMessage = `${url} is not ok. ${error}`;
+      const options: LogEntityOptions = {
+        level: LogSeverityLevel.high,
+        message: errorMessage,
+        origin: currentFileName,
+      };
+      const log = new LogEntity(options);
+      this.callLogs(log);
+
+      this.errorCallback && this.errorCallback(errorMessage);
+      return false;
+    }
+  }
+}
--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,6 +1,7 @@
 import { envs } from "../config/plugins/envs.plugin";
 import { LogSeverityLevel } from "../domain/entities/log.entity";
 import { CheckService } from "../domain/use-cases/checks/checks-service";
+import { CheckServiceMultiple } from "../domain/use-cases/checks/checks-service-multiple";
 import { sendEmailLogs } from "../domain/use-cases/email/send-email-logs";
 import { FileSystemDataSource } from "../infrastructure/datasources/file-system.datasource";
 import { MongoLogDatasource } from "../infrastructure/datasources/mongo-log.datasource";
@@ -9,9 +10,9 @@ import { LogRepositoryImpl } from "../infrastructure/repositories/log.repository
 import { CronService } from "./cron/cron-service";
 import { EmailServices } from "./email/email.services";
 
-const logRepository = new LogRepositoryImpl(
-  // new FileSystemDataSource(),
-  // new MongoLogDatasource(),
+const fsLogRepository = new LogRepositoryImpl(new FileSystemDataSource());
+const mongoLogRepository = new LogRepositoryImpl(new MongoLogDatasource());
+const postgresLogRepository = new LogRepositoryImpl(
   new PostgresLogDatasource()
 );
 const emailServices = new EmailServices();
@@ -29,14 +30,14 @@ export class Server {
     // const logs = await logRepository.getLogs(LogSeverityLevel.low);
     // console.log(logs);
 
-    // CronService.createJob("*/5 * * * * *", () => {
-    //   const url: string = "https://google.com";
+    CronService.createJob("*/5 * * * * *", () => {
+      const url: string = "https://google.com";
 
-    //   new CheckService(
-    //     logRepository,
-    //     () => console.log(`${url} is ok`),
-    //     (error) => console.log(error)
-    //   ).execute(url);
-    // });
+      new CheckServiceMultiple(
+        [fsLogRepository, mongoLogRepository, postgresLogRepository],
+        () => console.log(`${url} is ok`),
+        (error) => console.log(error)
+      ).execute(url);
+    });
   }
 }
